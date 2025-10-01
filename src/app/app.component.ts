22d7b301432f5920bf6dfe4e86a5bf857a856ba7
vasavifullstack <vasavi.fullstack@gmail.com>
2025-10-01 21:27:10 +0530

    ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  roles=2;
+  CustomerID=102;
   BankName="HDFC";
   BankAccount=1234;
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CompAComponent } from './CompA/compA.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { CompAComponent } from './CompA/compA.component';
     CompAComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]   // 1 st batsman Abhishek sharma

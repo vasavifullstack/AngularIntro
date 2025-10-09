a7c03dfe3e0688c950de06f44993e9206b4cbc9c
vasavifullstack <vasavi.fullstack@gmail.com>
2025-10-09 19:16:01 +0530

    content

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,ContentChild,ElementRef,ViewChild,TemplateRef,ViewContainerRef} from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+@ViewChild('paragraph') p1 : ElementRef
+@ViewChild('pressword') s1 : ElementRef
+@ViewChild('firstName') firstName : ElementRef
+
+@ContentChild('surName') surname : ElementRef
+@ViewChild('testTemplate') template1 : TemplateRef<any>;
+btnClick(){
+  console.log(this.p1);
+  console.log(this.p1.nativeElement);
+}
+presskey(){
+  console.log(this.s1);
+  console.log(this.s1.nativeElement);
+  console.log(this.firstName.nativeElement);
+  console.log(this.template1);
 
 }
+
+  
+  @ViewChild('containerRef' , { read:ViewContainerRef })  containerRef : ViewContainerRef;
+  @ViewChild('containerRef')                                 v1 : ViewContainerRef;  // v1 is the element
+  
+
+  btnClickme(){
+    console.log(this.containerRef);
+     this.containerRef.
+     insert(this.template1.createEmbeddedView({"Firstname":"John" , "lastname" : "smith" , "Hometown" : "UK"}));
+    // console.log(this.v1);
+     //type 
+     //console.log(this.template1);
+  }
+
+
+
+}
+
